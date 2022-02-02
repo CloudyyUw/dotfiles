1636257fd4d05d5dfa97481e24379cd2bc37e7ce
CloudyyUw <cloudyy.contact@gmail.com>
2022-02-02 19:05:53 -0300

    setup ssh, git, albert and bashrc/zshrc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # BR Arch mirrors
 MIRRORS_URL=(
   'Server = http://ca.us.mirror.archlinux-br.org/$repo/os/$arch'
@@ -40,18 +41,92 @@ setup_mirrorlist()
   pacman -Syy
 }
 
+setup_git()
+{
+  
+  read -p "Git email: " GIT_EMAIL
+  read -p "Git username: " GIT_USER
+
+  cat > .gitconfig << EOF
+[user]
+	email = $GIT_EMAIL
+	name = $GIT_USER
+[core]
+	editor = nvim
+
+EOF
+  cp .gitconfig ~/
+}
+
+setup_ssh()
+{
+  cp .ssh ~/.ssh
+}
+
+setup_bash_zshrc_others()
+{
+  BASHPATH="~/.bashrc"
+  ZSHRCPATH="~/.zshrc"
+  
+  # > = create file
+  # >> append
+  cat > $BASHPATH << "EOF"
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+EOF
+  
+  cat >> $ZSHRCPATH << "EOF"
+env=~/.ssh/agent.env
+
+agent_load_env () { test -f "$env" && . "$env" >| /dev/null ; }
+
+agent_start () {
+    (umask 077; ssh-agent >| "$env")
+    . "$env" >| /dev/null ; }
+
+agent_load_env
+
+# agent_run_state: 0=agent running w/ key; 1=agent w/o key; 2=agent not running
+agent_run_state=$(ssh-add -l >| /dev/null 2>&1; echo $?)
+
+if [ ! "$SSH_AUTH_SOCK" ] || [ $agent_run_state = 2 ]; then
+    agent_start
+    ssh-add
+elif [ "$SSH_AUTH_SOCK" ] && [ $agent_run_state = 1 ]; then
+    ssh-add
+fi
+
+unset env
+EOF
+
+  cp .albertignore ~/
+}
+
 while true; do 
   echo "1) Setup Nvim"
   echo "2) Setup Fonts"
   echo "3) Setup Mirrorlist [root]"
-  echo "4) Exit"
+  echo "4) Setup git"
+  echo "5) Setup ssh"
+  echo "6) Setup bashrc/zshrc (and others)"
+  echo "0) Exit"
   echo 
-  read -p "[1 ~ 4] -> " opt
+  read -p "[0 ~ 6] -> " opt
 
   case $opt in
     1 ) setup_nvim;;
     2 ) setup_fonts;;
     3 ) setup_mirrorlist;;
-    4 ) echo "Bye"; exit;;
+    4 ) setup_git;;
+    5 ) setup_ssh;;
+    6 ) setup_bash_zshrc_others;;
+    0 ) echo "Bye"; exit;;
   esac
 done
